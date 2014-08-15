23d73413be2697f846ed5b7d6cea230d1683f5f7
Jose Juarez <120photo@gmail.com>
2014-08-15 00:07:57 -0700

    Created associations between users and posts. schanged seeds to seed fake users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
 require 'faker'
 
-# create posts
+# Create Users
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation! # Keeps the app from sending confirmation to take addresses
+  user.save
+end
+users = User.all
 
+# create posts
 50.times do
   Post.create(
+    user:  users.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph
   )
@@ -14,11 +26,20 @@ posts = Post.all
 
 100.times do
 	Comment.create(
+    # user: users.sample, # uncomment when Users are associated with Comments
 		post: posts.sample,
 		body: Faker::Lorem.paragraph
 	)
 end
 
+# Updates first account with my info so I don't have to
+# create a account every time I seed new data.
+User.first.update_attributes(
+  email: '120photo@gmail.com',
+  password: 'password',
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} post created"
 puts "#{Comment.count} comments created"
