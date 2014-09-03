b9cdf72c19ad0238c791368c112ee2d0ad9f11b1
Jose Juarez <120photo@gmail.com>
2014-09-02 23:51:21 -0700

    added voting functionality, still needs to be cleaned up

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-   resources :users, only: [:update]
-  # resources :posts
-  # resources :topics
+  resources :users, only: [:update]
 
   resources :topics do
     resources :posts, except: [:index] do
       resources :comments, only: [:create, :destroy]
+
+      post '/up-vote' => 'votes#up_vote', as: :up_vote
+      post '/down-vote' => 'votes#down_vote', as: :down_vote
     end
   end
 
--- spec/vote_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Vote do
-  describe "validations" do
-    describe "value validation" do
-      it "only allows -1 or 1 as values" do
-        expect( Vote.new(value: 2) ).to eq(false) # your expectations here
-      end
-    end
-  end
-end
