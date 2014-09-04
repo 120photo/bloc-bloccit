6f6f11f462edfeb3b0370388596a063012fb5178
Jose Juarez <120photo@gmail.com>
2014-09-03 22:06:23 -0700

    cleaned up some code to be able to follow examples

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,14 +6,14 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    # @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
 
     if @comment.save
       flash[:notice] = "2¢ notted"
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "We were not paying attention so something went wrong, say that again..."
       render :new
