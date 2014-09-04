5b42ddc2a11b36213b083f08e58e610ac47f44af
Jose Juarez <120photo@gmail.com>
2014-09-04 00:21:27 -0700

    finished up voting feature

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,7 +18,14 @@ class Post < ActiveRecord::Base
     votes.sum(:value)
   end
 
-  default_scope { order('created_at DESC')}
+  def update_rank
+    age = (created_at - Time.new(1970,1,1)) / (60 * 60 *24)
+    new_rank = points + age
+
+    update_attribute(:rank, new_rank)
+  end
+
+  default_scope { order('rank DESC')}
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
