47f02dc860dbe3498990abd8935bb7cb11c30a95
Jose Juarez <120photo@gmail.com>
2014-09-04 19:14:10 -0700

    changed specs to deal with voting

--- app/models/post.rb
+++ app/models/post.rb
@@ -29,10 +29,9 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
-  after_create :create_vote
-
-  private
 
   def create_vote
     user.votes.create(calue: 1, post: self)
