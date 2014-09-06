69689153fc303cd8ca6383ceaa21fb42045ca080
Jose Juarez <120photo@gmail.com>
2014-09-06 08:06:39 -0700

    added favorite functions, controller, model, view buttons

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,31 @@
+class FavoritesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.build(post: @post)
+
+    authorize favorite
+    
+    if favorite.save
+      flash[:notice] = "Added to favorites"
+      redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "We could not add to your favorites, try again"
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find(params[:id])
+
+    authorize favorite
+
+    if favorite.destroy
+      flash[:notice] = "Favorite was removed"
+      redirect_to :back
+    else
+      flash[:error] = "Nope, try again"
+      redirect_to :back
+    end
+  end
+end
