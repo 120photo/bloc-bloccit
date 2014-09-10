28ad7034e2742078663951db196cfcbdf3a38a5b
Jose Juarez <120photo@gmail.com>
2014-09-09 19:38:51 -0700

    cleaned up _vote.html.erb view by converting some classes into a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,14 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render text).html_safe
   end
+
+  def vote_link_classes(post, vote)
+    if vote == 'up'
+      "glyphicon glyphicon-chevron-up #{(current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : '' }"
+    elsif vote == 'down'
+      "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : '' }"
+    else
+      flash[:error] = "there was an error voting"
+    end
+  end
 end
