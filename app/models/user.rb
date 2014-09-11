37320f72bf20eff42cfe9299ddd87aebad3006d1
Jose Juarez <120photo@gmail.com>
2014-09-10 20:13:31 -0700

    added a user index

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,15 @@ class User < ActiveRecord::Base
   def voted(post)
     votes.where(post_id: post.id).first
   end
+
+  def self.top_rated
+    self.select('users.*') # selects all user attributes
+        .select('COUNT(DISTINCT comments.id) AS comments_count') # count comments made by each user
+        .select('COUNT(DISTINCT posts.id) AS posts_count') # count posts made by user
+        .select('COUNT(DISTINCT comments.id) + COUNT(DISTINCT posts.id) AS rank') # Add the comment count to the post count and label the sum as "rank"
+        .joins(:posts)
+        .joins(:comments)
+        .group('users.id')
+        .order('rank DESC')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:update, :show]
+  resources :users, only: [:update, :show, :index]
 
   resources :topics do
     resources :posts, except: [:index], controller: 'topics/posts'
